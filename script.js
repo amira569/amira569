91e17cdc3674ce5cef2015ef2c34a5355cce2e4a
amira salah halawa <149798110+amira569@users.noreply.github.com>
2023-11-03 18:22:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,51 @@
+
+
 const wordText = document.querySelector(".word"),
 hintText = document.querySelector(".hint span"),
 timeText = document.querySelector(".time b"),
-inputField = document.querySelector("input"),
-refreshBtn = document.querySelector(".refresh-word"),
-checkBtn = document.querySelector(".check-word");
+input = document.querySelector(".input"),
+refreshButton = document.querySelector(".refreshButton"),
+checkButton = document.querySelector(".checkButton");
 
 let correctWord, timer;
 
-const initTimer = maxTime => {
-    clearInterval(timer);
+const initialTimer = (maxTime) => {
+    clearInterval(timer);//when initial time reach to max value clear the interval yo avoid repeating count
     timer = setInterval(() => {
         if(maxTime > 0) {
             maxTime--;
             return timeText.innerText = maxTime;
         }
-        alert(`Time off! ${correctWord.toUpperCase()} was the correct word`);
-        initGame();
-    }, 1000);
+        alert(` your time is over ,the correct answer is: ${correctWord.toUpperCase()} `);
+        initOfPage();
+    }, 1000 );
 }
 
-const initGame = () => {
-    initTimer(30);
-    let randomObj = words[Math.floor(Math.random() * words.length)];
-    let wordArray = randomObj.word.split("");
-    for (let i = wordArray.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
+const initOfPage = () => {
+    initialTimer(100);
+    let randomObjectsOfWords = words[Math.floor(Math.random() * words.length)];
+    let wordArray = randomObjectsOfWords.word.split("");// split word to char
+    for (let i = wordArray.length - 1; i > 0; i--) {    //take the chars after split and switch every two chars (switch)
+    let j = Math.floor(Math.random() * (i + 1));
         [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
     }
-    wordText.innerText = wordArray.join("");
-    hintText.innerText = randomObj.hint;
-    correctWord = randomObj.word.toLowerCase();;
-    inputField.value = "";
-    inputField.setAttribute("maxlength", correctWord.length);
+    wordText.innerText = wordArray.join(""); //chars to string after switch it
+    hintText.innerText = randomObjectsOfWords.hint;
+    correctWord = randomObjectsOfWords.word.toLowerCase();
+   
+   
 }
-initGame();
-
-const checkWord = () => {
-    let userWord = inputField.value.toLowerCase();
-    if(!userWord) return alert("Please enter the word to check!");
-    if(userWord !== correctWord) return alert(`Oops! ${userWord} is not a correct word`);
-    alert(`Congrats! ${correctWord.toUpperCase()} is the correct word`);
-    initGame();
+initOfPage();
+//to check if entered word is equal to the original word
+const checkedWord = () => {
+    let enteredWord = input.value.toLowerCase();
+    if(enteredWord=="") 
+    return alert("Enter A Word To Check!"); //if you clicked checkword btn without enter avalue
+    if(enteredWord == correctWord) 
+     //check if your answer is not correct?
+   return alert(`Bravo Amira ${correctWord.toUpperCase() }is the correct answer`);
+alert(`Sorry Amira ${enteredWord.toUpperCase()} is not the correct answer try again`);
+initOfPage();
 }
-
-refreshBtn.addEventListener("click", initGame);
-checkBtn.addEventListener("click", checkWord);
+refreshButton.addEventListener("click", initOfPage);
+checkButton.addEventListener("click", checkedWord);
